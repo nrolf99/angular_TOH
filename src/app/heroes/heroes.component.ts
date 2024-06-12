95ed6437a116c1230cfcd29e8b86f94c1522c4c4
nrolf99 <nrolf99@gmail.com>
2024-06-12 18:42:09 -0500

    initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  hero: Hero = {
+    name: 'Windstorm',
+    id: 0,
+  }
+  ;
+}
